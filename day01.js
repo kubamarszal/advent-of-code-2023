254de99cab41945b6bf5e0653aabfe82feb0c86d
Jakub Marszal <marszal.kuba@gmail.com>
2023-12-01 18:37:54 +0100

    Refactor + STAR2 solution

--- day01.js
+++ day01.js
@@ -1,29 +1,40 @@
-const input = require('./day01-input')
+const read = require('./day01-input')
 
-const main = () => {
-    let sum = 0;
+const TABLE = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'twone': '21',
+    'eightwo': '82',
+    'eighthree': '83',
+    'oneight': '18',
+    'threeight': '38',
+    'fiveight': '58',
+    'nineight': '98',
+    'sevenine': '79',
+}
+const one = '[1-9]';
+const two = 'twone|eightwo|eighthree|oneight|threeight|fiveight|nineight|sevenine|one|two|three|four|five|six|seven|eight|nine|[1-9]';
+
+const transit = (input, regexp) => {
+    let result = 0;
     input.forEach(item => {
-        let l = 0, r = item.length - 1;
-        let number = '';
-
-        while(number.length < 1) {
-            if(Number.isInteger(1 * item[l]))
-                number += item[l];
-            
-            l++;
-        }
-
-        while(number.length > 0 && number.length < 2) {
-            if(Number.isInteger(1 * item[r])) 
-                number += item[r];
-        
-            r--;
-        }
-
-        sum += (1 * number);
+        let str = '';
+        const arr = Array(...item.matchAll(regexp));
+        const first = arr[0][0];
+        const last = arr[arr.length - 1][0];
+        str += Number.isNaN(first * 1) ? TABLE[first] : first;
+        str += Number.isNaN(last * 1) ? TABLE[last] : last;
+        result += str.length > 2 ? `${str[0]}${str[2]}` * 1 : str * 1;
     });
-
-    return sum;
+    return result;
 }
 
-console.log(main())
+console.log(`FIRST STAR SOLUTION: ${transit(read, one)}`);
+console.log(`SECOND STAR SOLUTION: ${transit(read, two)}`);
