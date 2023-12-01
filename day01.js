c8ff9695099acf37b907df5254349850f4d43b53
Jakub Marszal <marszal.kuba@gmail.com>
2023-12-01 13:14:15 +0100

    Day 01 - Star 01 solved

--- day01.js
+++ day01.js
@@ -1,1 +1,29 @@
-console.log("Hello");
+const input = require('./day01-input')
+
+const main = () => {
+    let sum = 0;
+    input.forEach(item => {
+        let l = 0, r = item.length - 1;
+        let number = '';
+
+        while(number.length < 1) {
+            if(Number.isInteger(1 * item[l]))
+                number += item[l];
+            
+            l++;
+        }
+
+        while(number.length > 0 && number.length < 2) {
+            if(Number.isInteger(1 * item[r])) 
+                number += item[r];
+        
+            r--;
+        }
+
+        sum += (1 * number);
+    });
+
+    return sum;
+}
+
+console.log(main())
